60531b218fb9f3a7f6783020a3d60010a54b5cb9
SILVERU-GOLDERU <rorgzen@gmail.com>
2024-10-30 21:12:59 +0000

    BIIG  a bit of bomb/more map/camera/movement/jump/animations/more?

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class Bomb : MonoBehaviour
-{
+public class Bomb : MonoBehaviour {
 
     private Rigidbody2D _rb;
     [SerializeField] private Collider2D _col;
@@ -22,13 +21,13 @@ public class Bomb : MonoBehaviour
     [SerializeField] private Collider2D _groundCheck;
     [SerializeField] private LayerMask groundMask;
     [SerializeField] private LayerMask playerMask;
+    [SerializeField] private LayerMask dynamicObjectMask;
     [SerializeField] private bool touchedSomething = false;
     private Rigidbody2D playerRigidbody2D;
     private Vector2 dif;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
 
         _rb = GetComponent<Rigidbody2D>();
         _col = GetComponent<CircleCollider2D>();
@@ -38,36 +37,26 @@ public class Bomb : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
 
         clamping();
-
     }
 
-    private void FixedUpdate()
-    {
+    private void FixedUpdate() {
         
         checkGround();
-
     }
 
-    public void triggerExplosion()
-    {
-
+    public void triggerExplosion() {
+        
         playerRigidbody2D = FindAnyObjectByType<PlayerController>().gameObject.GetComponent<Rigidbody2D>();
 
-        if (_explosionArea.IsTouchingLayers(playerMask) == true)
-        {
+        if (_explosionArea.IsTouchingLayers(playerMask) == true) {
             
             playerRigidbody2D.gravityScale = 8;
-
             FindAnyObjectByType<PlayerController>().atHighSpeeds = true;
-
             dif = FindAnyObjectByType<PlayerController>().gameObject.GetComponent<Transform>().position - transform.position;
-
             playerRigidbody2D.velocity = dif.normalized * explosionVelocity;
-
             //print(playerRigidbody2D.velocity);
 
         }
@@ -75,64 +64,47 @@ public class Bomb : MonoBehaviour
         FindAnyObjectByType<SpawningBomb>().bombCount = 0;
         FindAnyObjectByType<SpawningBomb>().bombCanDetonate = false;
         Destroy(gameObject);
-    
     }
 
-    private void clamping()
-    {
+    private void clamping(){
 
-        if (_rb.velocity.y < fallingTopSpeed)
-        {
-
+        if (_rb.velocity.y < fallingTopSpeed){
+            
             _rb.velocity = new Vector2(_rb.velocity.x, fallingTopSpeed);
-
         }
 
-        if (_rb.velocity.x > 0.15)
-        {
+        if (_rb.velocity.x > 0.15){
 
             _rb.velocity += new Vector2(-decceleration, _rb.velocity.y) * Time.deltaTime;
-
-        }
-        else if (_rb.velocity.x < -0.15)
-        {
-
+            
+        }else if (_rb.velocity.x < -0.15){
+            
             _rb.velocity += new Vector2(decceleration, _rb.velocity.y) * Time.deltaTime;
-
         }
 
-        if (_rb.velocity.x <= 0.15 && _rb.velocity.x >= -0.15)
-        {
+        if (_rb.velocity.x <= 0.15 && _rb.velocity.x >= -0.15){
 
             _rb.velocity = new Vector2(0, _rb.velocity.y);
-
         }
 
     }
 
-    private void checkGround()
-    {
+    private void checkGround(){
 
-        if (_groundCheck.IsTouchingLayers(groundMask) == true)
-        {
+        if (_groundCheck.IsTouchingLayers(groundMask) == true) {
 
             _rb.velocity = new Vector2(_rb.velocity.x, 0);
             //print("ground");
-
         }
     
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
+    private void OnCollisionEnter2D(Collision2D collision){
         
-        if (touchedSomething == false)
-        {
+        if (touchedSomething == false) {
 
             touchedSomething = true;
             FindAnyObjectByType<SpawningBomb>().GetComponent<SpawningBomb>().bombCanDetonate = true;
-
         }
-
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,12 +6,13 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour{
+    
     private Rigidbody2D _rb;
     private Collider2D _col;
     private RaycastHit2D _groundCheck;
-     private MovingPlatform currentPlatform; 
+    private MovingPlatform currentPlatform; 
+    private Animator animator;
     
     private float horizontal_input;
 
@@ -26,11 +27,19 @@ public class PlayerController : MonoBehaviour
     private float jumpBufferTimer;
     [SerializeField] private float coyoteTime = 0.1f;
     private float coyoteTimeTimer;
+    [SerializeField] private float maxJumpHeight = 1f; // Maximum height for jumping
+    private float initialJumpY; // Store the initial Y position when the jump starts
+
 
     [Header("Physics")]
     [SerializeField] private float fallingTopSpeed = 20f;
     [SerializeField] private float gravity = 20f;
 
+    [Header("Gravity Control")]
+    [SerializeField] private float gravityIncreaseRate = 5f; // Rate at which gravity increases
+    [SerializeField] private float maxGravityScale = 100f;    // Maximum gravity scale
+    private float currentGravityScale; 
+    
     [Header("Details")]
     [SerializeField] private LayerMask groundMask;
     [SerializeField] public bool isFacingRight;
@@ -41,10 +50,11 @@ public class PlayerController : MonoBehaviour
     private float atHighSpeedTimer;
 
     // Start is called before the first frame update
-    private void Start()
-    {
+    private void Start(){
+        
         _rb = GetComponent<Rigidbody2D>();
         _col = GetComponent<BoxCollider2D>();
+        animator = GetComponent<Animator>();
         isFacingRight = true;
         fallingTopSpeed *= -1;
         atHighSpeeds = false;
@@ -52,11 +62,9 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    private void Update()
-    {
+    private void Update(){
 
-        if (!atHighSpeeds)
-        {
+        if (!atHighSpeeds){
 
             movement();
 
@@ -68,162 +76,148 @@ public class PlayerController : MonoBehaviour
 
         checkGround();
 
-        turnCHeck();
+        turnCheck();
 
         highSpeeds();
 
-    }
-
-    private void movement()
-    {
-
-        horizontal_input = Input.GetAxisRaw("Horizontal");
-
-        if (horizontal_input != 0 && _rb.velocity.x < topSpeed && _rb.velocity.x > -topSpeed)
-        {
-
-            _rb.velocity += new Vector2(horizontal_input * acceleration,0) * Time.deltaTime;
-
-        } else if (horizontal_input != 0 && _rb.velocity.x > topSpeed)
-        {
-
-            _rb.velocity = new Vector2(topSpeed, _rb.velocity.y);
-
-        }else if (horizontal_input != 0 && _rb.velocity.x < -topSpeed)
-        {
-
-            _rb.velocity = new Vector2(-topSpeed, _rb.velocity.y);
-
-        }
-
-        if (horizontal_input <= 0 && _rb.velocity.x > 0.15)
-        {
-
-            _rb.velocity += new Vector2(-decceleration, 0) * Time.deltaTime;
-
-        } else if (horizontal_input >= 0 && _rb.velocity.x < -0.15)
-        {
-
-            _rb.velocity += new Vector2(decceleration, 0) * Time.deltaTime;
-
-        }
-
-        if (horizontal_input == 0 && _rb.velocity.x <= 0.15 && _rb.velocity.x >= -0.15)
-        {
-
-            _rb.velocity = new Vector2(0, _rb.velocity.y);
-
-        }
+        gravityControl();
 
     }
 
-    private void jump()
+    private void gravityControl()
     {
-
-        //WHEN WE PRESS THE JUMP BUTTON
-
-        if (jumpBufferTimer > 0 && coyoteTimeTimer > 0)
+        if (!isGrounded)
         {
-
-            _rb.velocity = new Vector2(_rb.velocity.x, jumpPower);
-
-            jumpBufferTimer = 0;
-        
-        }
-
-        //WHEN WE RELEASE THE JUMP BUTTON
-
-        if (Input.GetButtonUp("Jump") && _rb.velocity.y >= 0)
-        {
-
-            _rb.gravityScale = gravity * 3;
-
-            coyoteTimeTimer = 0f;
-
-
-        }
-
-        //COYOTE TIME
-
-        if (isGrounded)
-        {
-
-            coyoteTimeTimer = coyoteTime;
-
-        }
-        else 
-        {
-
-            coyoteTimeTimer -= Time.deltaTime;
-
-
-        }
-
-        //JUMP BUFFERING
-
-        if (Input.GetButtonDown("Jump"))
-        {
-
-            jumpBufferTimer = jumpBufferTime;
-
+            // Increase the gravity scale while falling
+            currentGravityScale += gravityIncreaseRate * Time.deltaTime;
+            currentGravityScale = Mathf.Min(currentGravityScale, maxGravityScale); // Limit the gravity scale
+            _rb.gravityScale = currentGravityScale; // Apply the current gravity scale
         }
         else
         {
-
-            jumpBufferTimer -= Time.deltaTime;
-
-
-        }
-
-        //RESET GRAVITY
-
-        if (isGrounded)
-        {
-
-            _rb.gravityScale = gravity;
-
+            // Reset the gravity scale when grounded
+            currentGravityScale = gravity; // Or reset to your original gravity value
         }
     }
+    private void movement() {
 
-    private void clampFalling() 
-    {
-    
-        if (_rb.velocity.y < fallingTopSpeed) 
-        {
-
-            _rb.velocity = new Vector2(_rb.velocity.x, fallingTopSpeed);
+        horizontal_input = Input.GetAxisRaw("Horizontal");
         
+         //if the player is pressing left or right (horizontal_input != 0) the velocity increases either 1/-1
+        if (horizontal_input != 0 && Mathf.Abs(_rb.velocity.x) < topSpeed) {
+        
+            _rb.velocity += new Vector2(horizontal_input * acceleration, 0) * Time.deltaTime;
+            animator.SetBool("Move", true); 
+            
+         //if the player is trying to move in right direction while already exceeding the topSpeed. if true then caps.
+        }else if (horizontal_input > 0 && _rb.velocity.x > topSpeed){
+        
+            _rb.velocity = new Vector2(topSpeed, _rb.velocity.y);
+            animator.SetBool("Move", true);
+            
+         //if the player is trying to move in left direction while already exceeding the topSpeed. if true then caps.
+        }else if (horizontal_input < 0 && _rb.velocity.x < -topSpeed) {
+            
+            _rb.velocity = new Vector2(-topSpeed, _rb.velocity.y);
+            animator.SetBool("Move", true); 
+            
+         // Set move to false when idle and zeroes velocity immediately to prevent sliding
+        }else if (horizontal_input == 0) {
+            
+            _rb.velocity = new Vector2(0, _rb.velocity.y); 
+            //animator.SetBool("Move", false);
         }
-    
+        
+        
+         //slowdown code if no right input is made but still moving
+        if (horizontal_input <= 0 && _rb.velocity.x > 0.15){
+            
+            _rb.velocity += new Vector2(-decceleration, 0) * Time.deltaTime;
+            animator.SetBool("move", true);
+        
+         //slowdown code if no left input is made but still moving
+        }else if (horizontal_input >= 0 && _rb.velocity.x < -0.15){
+
+            _rb.velocity += new Vector2(decceleration, 0) * Time.deltaTime;
+            animator.SetBool("move", true);
+        }
+        
+        
+         //slowdown code if no input is made but still moving
+        if (horizontal_input == 0 && _rb.velocity.x <= 0.15 && _rb.velocity.x >= -0.15){
+            
+            _rb.velocity = new Vector2(0, _rb.velocity.y);
+            animator.SetBool("Move", false);
+        }
     }
 
-    private void checkGround() 
-    {
+    private void jump(){
+        
+         //JUMP BUFFERING
+        if (Input.GetButtonDown("Jump")){
+            jumpBufferTimer = jumpBufferTime;
+            initialJumpY = transform.position.y; 
+        }else{
+            jumpBufferTimer -= Time.deltaTime;
+        }
+        
+         //WHEN WE PRESS THE JUMP BUTTON
+        if (jumpBufferTimer > 0 && coyoteTimeTimer > 0){
+            _rb.velocity = new Vector2(_rb.velocity.x, jumpPower);
+            jumpBufferTimer = 0;
+        }
+        
+         //STOP BIG "FLOATING" WHEN AFTER REACHING MAX HEIGHT
+        if (!isGrounded && Input.GetButton("Jump")) {
+            if (transform.position.y >= initialJumpY + maxJumpHeight) {
+                _rb.velocity = new Vector2(_rb.velocity.x, jumpPower * 3f);
+            }
+        }
+        
+         //COYOTE TIMER
+        if (isGrounded){
+            coyoteTimeTimer = coyoteTime;
+        }else{
+            coyoteTimeTimer -= Time.deltaTime;
+        }
+
+         //WHEN WE RELEASE THE JUMP BUTTON
+        if (Input.GetButtonUp("Jump") && _rb.velocity.y >= 0){
+            _rb.gravityScale = gravity * 1.6f;
+            coyoteTimeTimer = 0f;
+        }
+        
+         //RESET GRAVITY
+        if (isGrounded){
+            _rb.gravityScale = gravity;
+        }
+    }
+
+    private void clampFalling(){
+        
+        if (_rb.velocity.y < fallingTopSpeed){
+            _rb.velocity = new Vector2(_rb.velocity.x, fallingTopSpeed);
+        }
+    }
+
+    private void checkGround() {
 
         Color _color = Color.green;
-
         _groundCheck = Physics2D.BoxCast(_col.bounds.center, _col.bounds.size, 0f, Vector2.down, 0.1f, groundMask);
-
         isGrounded = _groundCheck.collider != null ? true : false; 
 
         
-
-        if (_groundCheck.collider != null)
-        {
-
+        if (_groundCheck.collider != null){
+            
             _color = Color.red;
-
-        }
-        else if (coyoteTimeTimer > 0)
-        {
-
+            
+        }else if (coyoteTimeTimer > 0){
+            
             _color = Color.blue;
-
-        }else if (jumpBufferTimer > 0)
-        {
-
+            
+        }else if (jumpBufferTimer > 0){
+            
             _color = Color.cyan;
-
         }
 
         //Draws the _groundCheck lower and side bounds to give feedback while testing in the editor
@@ -234,60 +228,46 @@ public class PlayerController : MonoBehaviour
 
     }
 
-    private void turnCHeck()
-    {
+    private void turnCheck(){
 
-        if (isFacingRight && horizontal_input < 0)
-        {
+        if (isFacingRight && horizontal_input < 0){
 
             isFacingRight = false;
             GetComponentInChildren<SpriteRenderer>().flipX = true;
 
-        } else if (!isFacingRight && horizontal_input > 0)
-        {
-        
+        }else if (!isFacingRight && horizontal_input > 0){
+            
             isFacingRight = true;
             GetComponentInChildren<SpriteRenderer>().flipX = false;
-
         }
-        
     }
 
-    void highSpeeds()
-    {
+    void highSpeeds(){
     
-        if (atHighSpeeds)
-        {
+        if (atHighSpeeds){
 
             atHighSpeedTimer -= Time.deltaTime;
-
         }
         
-        if (atHighSpeedTimer < 0 && atHighSpeeds)
-        {
+        if (atHighSpeedTimer < 0 && atHighSpeeds){
 
             atHighSpeeds = false;
-
             atHighSpeedTimer = atHighSpeedTime;
-
         }
-    
     }
     
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("MovingPlatform"))
-        {
+    private void OnCollisionEnter2D(Collision2D collision) {
+        
+        if (collision.gameObject.CompareTag("MovingPlatform")) {
+            
             currentPlatform = collision.gameObject.GetComponent<MovingPlatform>();
         }
     }
 
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("MovingPlatform"))
-        {
+    private void OnCollisionExit2D(Collision2D collision) {
+        
+        if (collision.gameObject.CompareTag("MovingPlatform")){
             currentPlatform = null;
         }
     }
-
 }
